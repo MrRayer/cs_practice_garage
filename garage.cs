263455767140e9780078ed1f21b4c8bc6cfcd1c8
MrRayer <rodrigo.ces.b@gmail.com>
2022-06-24 19:05:01 -0300

    first commit

---
+++ garage.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace garage_cspractice
+{
+    internal static class garage
+    {
+        static List<vehicle> garage_list = new List<vehicle>();
+
+        static public void Start()
+        {
+            Console.WriteLine("Escriba el numero de la accion a realizar");
+            Console.Write("1 - Ingreso de vehiculo, 2 - Egreso de vehiculo, 3 - Realizar partial checkout, 4 - Salir: ");
+            string answer = Console.ReadLine();
+            answer_check(answer);
+        }
+
+        static private void answer_check(string answer)
+        {
+            switch (answer) 
+            {
+                case "1":
+                    Console.WriteLine("1 selected");
+                    garage_list.Add(new vehicle());
+                    Console.WriteLine("Vehiculo ingresado");
+                    Start();
+                    break;
+                case "2":
+                    Console.WriteLine("2 selected");
+                    Console.Write("Introduzca el dni del dueño del vehiculo egresante: ");
+                    string dni = Console.ReadLine();
+                    int index = Dni_check(dni);
+                    if (index == -1) { Start(); };
+                    Console.WriteLine($"Dueño {garage_list[index].owner} encontrado");
+                    garage_list[index].checkout();
+                    garage_list.RemoveAt(index);
+                    Start();
+                    break;
+                case "3":
+                    Console.WriteLine("3 selected");
+                    Console.Write("Ingrese la hora a revizar: ");
+                    int check_time = Convert.ToInt32(Console.ReadLine());
+                    int check_ammount = 0;
+                    int vehicle_count = 0;
+                    foreach (vehicle x in garage_list)
+                    {
+                        check_ammount += x.partial_check(check_time);
+                        vehicle_count++;
+                    }
+                    Console.WriteLine($"{vehicle_count} guardados en el garage");
+                    Console.WriteLine($"{check_ammount}$ hasta ahora en vehiculos guardados");
+                    Start();
+                    break;
+                case "4":
+                    return;
+                case "test":
+                    Console.WriteLine("Vehiculos ingresados:");
+                    foreach (vehicle x in garage_list)
+                    {
+                        Console.WriteLine(x.type);
+                    }
+                    Console.WriteLine("Fin de vehiculos ingresados.");
+                    Start();
+                    break;
+                default:
+                    Console.WriteLine("Opcion incorrecta");
+                    Start();
+                    break;
+            }
+        }
+
+        static private int Dni_check(string dni)
+        {
+            int counter = 0;
+            foreach (vehicle x in garage_list)
+            {
+                if (x.owner_dni == dni)
+                {
+                    return counter;
+                }
+                else { counter++; }
+            }
+            Console.WriteLine("dni no encontrado en la lista de vehiculos");
+            return -1;
+        }
+    }
+}
---
+++ vehicle.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace garage_cspractice
+{
+    internal class vehicle
+    {
+        public string owner;
+        public string owner_dni;
+        public string type;
+        private string patent;
+        private string mode;
+        private int in_time;
+        private int out_time;
+        public string Mode 
+        { 
+            get { return mode; } 
+            set 
+            { 
+                if (value == "fix" || value == "mobile") { mode = value; } 
+                else
+                {
+                    Console.Write("modo incorrecto, ingrese un modo de estacionamiento valido (fix, mobile): ");
+                    Mode = Console.ReadLine();
+                }
+            } 
+        }
+
+        public vehicle()
+        {
+            Console.Write("Introduzca el nombre del propietario: ");
+            owner = Console.ReadLine();
+            Console.Write("Introduzca el dni del propietario: ");
+            owner_dni = Console.ReadLine();
+            Console.Write("Introduzca el tipo de vehiculo: ");
+            type = Console.ReadLine();
+            Console.Write("Introduzca la patente del vehiculo: ");
+            patent = Console.ReadLine();
+            Console.Write("Introduzca el modo de estacionamiento (fix, mobile): ");
+            Mode = Console.ReadLine();
+            Console.Write("Introduzca la hora de ingreso, formato ( hhmm ex: 1400 -> 2:00 pm ): ");
+            in_time = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public int partial_check(int time)
+        {
+            int pay = time;
+            if (mode == "fix")
+            {
+                pay -= in_time;
+                pay *= 2;
+                pay += 500;
+                return pay;
+            }
+            else
+            {
+                pay -= in_time;
+                pay *= 2;
+                return pay;
+            }
+        }
+
+        public void checkout() 
+        {
+            Console.Write("Introduzca la hora de salida, formato ( hhmm ex: 1400 -> 2:00 pm ): ");
+            out_time = Convert.ToInt32(Console.ReadLine());
+            int pay = out_time;
+            if (mode == "fix")
+            {
+                Console.WriteLine("type fix");
+                pay -= in_time;
+                pay *= 2;
+                pay += 500;
+                Console.WriteLine(pay);
+                return;
+            }
+            else
+            {
+                Console.WriteLine("type mobile");
+                pay -= in_time;
+                pay *= 2;
+                Console.WriteLine(pay);
+                return;
+            }
+        }
+    }
+}
