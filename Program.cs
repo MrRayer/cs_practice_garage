ab722be1c4022f8e2dc2f656d0ead52df9a3801d
MrRayer <rodrigo.ces.b@gmail.com>
2022-06-24 19:08:07 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace garage_cspractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            garage.Start();
+        }
+    }
+}
